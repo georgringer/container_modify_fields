600ede39f6aa8ac8608c23e59c6d4be4cc131263
Georg Ringer <georg.ringer@gmail.com>
2022-02-15 12:36:56 +0100

    [TASK] Improve version info

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,6 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
+            'depends' => ['typo3' => '9.5.0-11.99.99'],
             'container' => '1.4.0-1.99.99',
         ],
     ],
