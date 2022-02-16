c7d9785502de499552605c8de373e92de7cc579e
Georg Ringer <georg.ringer@gmail.com>
2022-02-16 13:24:20 +0100

    [BUGFIX] Fix dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,18 +1,20 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Hide fields of elements inside container',
-    'description' => '',
+    'title' => 'Modify fields of elements inside container',
+    'description' => 'Manipulate content elements which are children inside a EXT:container container',
     'category' => 'be',
     'author' => 'Georg Ringer',
     'author_email' => 'mail@ringer.it',
     'state' => 'beta',
     'clearCacheOnLoad' => true,
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'constraints' => [
         'depends' => [
-            'depends' => ['typo3' => '9.5.0-11.99.99'],
-            'container' => '1.4.0-1.99.99',
+            'depends' => [
+                'typo3' => '9.5.0-11.99.99',
+                'container' => '1.4.0-1.99.99',
+            ],
         ],
     ],
 ];
