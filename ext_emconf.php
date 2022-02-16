4605566e275bef6e1438a0c167e9c104e264df00
Georg Ringer <georg.ringer@gmail.com>
2022-02-16 13:26:02 +0100

    [BUGFIX] Followup

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,13 +8,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'mail@ringer.it',
     'state' => 'beta',
     'clearCacheOnLoad' => true,
-    'version' => '1.0.1',
+    'version' => '1.0.2',
     'constraints' => [
         'depends' => [
-            'depends' => [
-                'typo3' => '9.5.0-11.99.99',
-                'container' => '1.4.0-1.99.99',
-            ],
+            'typo3' => '9.5.0-11.99.99',
+            'container' => '1.4.0-1.99.99',
         ],
     ],
 ];
